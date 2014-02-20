4725e1acc448dc2d995971dad63997d7258cf18b
keithGrout <keiwi.g@gmail.com>
2014-02-20 14:37:21 -0800

    added arrays and objects to be stringified

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,15 +4,32 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // base case
-  // is obj a string
   
   // is an obj an array
   // is an obj an object
   // if obj is an array or object, is it nested
   
   if(typeof obj === "boolean" || typeof obj === "number" || obj === null){ return String(obj); }
+ 
+  if(typeof obj === "string") {return '"'+ obj + '"'; }
   
-  console.log(obj + " " + typeof obj);
+  if( Array.isArray(obj) ){
+    obj = obj.map(stringifyJSON);
+    return '[' + obj + ']';
+  }
+  
+ 
+  if (typeof obj === 'object') {
+    var arr = [];
+    for (item in obj) {
+      var key = stringifyJSON(item);
+      var value = stringifyJSON(obj[item]);
+      if (value) arr.push(key + ':' + value);
+    }
+    return '{' + arr.join() + '}';
+  }
+
+  else return undefined;
   
   
 };
