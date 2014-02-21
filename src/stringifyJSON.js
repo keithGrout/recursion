c9bca0c3c26371973722ac145632948922cdec5a
keithGrout <keiwi.g@gmail.com>
2014-02-20 16:12:27 -0800

    removed some comments

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,16 +3,13 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
+
   // base case
-  
-  // is an obj an array
-  // is an obj an object
-  // if obj is an array or object, is it nested
-  
   if(typeof obj === "boolean" || typeof obj === "number" || obj === null){ return String(obj); }
  
   if(typeof obj === "string") {return '"'+ obj + '"'; }
   
+  // recursive case
   if( Array.isArray(obj) ){
     obj = obj.map(stringifyJSON);
     return '[' + obj + ']';
