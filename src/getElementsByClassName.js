913028bc601e28e636cab367f5b02d9d5488bb5a
Hack Reactor Students <keiwi.g@gmail.com>
2014-04-14 12:45:57 -0700

    updated getElementsByClassName without inner function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,28 +4,57 @@
 // };
 
 // But instead we're going to implement it from scratch:
-  
-var getElementsByClassName = function(className) {
-  
-	var body = document.body;
-	var elements = [];
-	
-	function walkDom(node, className){
-	  if(node.nodeType === document.ELEMENT_NODE){ 
-		if(node.classList.contains(className)){
-	  		elements.push(node);
-	  	}
-	  	for(var i = 0; i < node.childNodes.length; i++){  	
-	  		walkDom(node.childNodes[i], className);
-	  	}
-	  }
-	  
-	}
 
-	walkDom(body, className);
-	return elements;
-	
-  
+var getElementsByClassName = function(className, node) {
+  var results = [];
+
+  node = node || document.body;
+
+if (node.classList) {
+	for(var i = 0; i < node.classList.length; i++){
+		if(node.classList[i] === className){
+			results.push(node);
+		}
+	}
+}
+
+
+ if (node.hasChildNodes) {
+ 	for (var i = 0; i < node.children.length; i++) {
+ 		results = results.concat(getElementsByClassName(className, node.children[i]));
+ 	}
+ }
+
+
+  return results;
 };
 
 
+
+
+
+
+
+
+
+
+
+
+/*
+var body = document.body;
+	var elements = [];
+
+	function walkDom(node, className){
+	  if(node.nodeType === document.ELEMENT_NODE){
+		if(node.classList.contains(className)){
+	  		elements.push(node);
+	  	}
+	  	for(var i = 0; i < node.childNodes.length; i++){
+	  		walkDom(node.childNodes[i], className);
+	  	}
+	  }
+
+	}
+
+	walkDom(body, className);
+	return elements;*/
